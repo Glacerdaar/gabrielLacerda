423658d90556dc036fad64a054a281cb8749aa2d
Gabriel <glacerdaar@gmail.com>
2024-01-05 17:06:03 -0300

    Erro identificado e resolvido

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Navbar from "./components/Navbar/Navbar"
 import Hero from "./components/Hero/Hero"
 import About from "./components/About/about"
 import Skill from "./components/Skills/skill"
-import Projects from "./components/Projects/projects"
 import Contact from "./components/Contact/Contact"
 import Footer from "./components/Footer/Footer"
+import Projects from "./components/Projects/projects"
 
 function App() {
 
