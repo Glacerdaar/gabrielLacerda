a794f5a7025a4c8a1002128671c558321474ff9b
Gabriel <glacerdaar@gmail.com>
2024-01-05 19:53:12 -0300

    Alteração no css do components Project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,4 @@
 import styles from "./projects.module.css"
-// import React from 'react'
-// import projects from "../../data/projects.json"
-// import { getImageUrl } from "../../utils"
 
 const Projects = () => {
   return (
